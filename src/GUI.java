0b2d880f3757d75e8928d3212f338ffd78712f3c
Carlos Venkman <cvenkman@si-f3.kzn.21-school.ru>
2022-01-24 22:08:29 +0300

    fix button

--- src/GUI.java
+++ src/GUI.java
@@ -2,28 +2,38 @@ import javax. swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public final class GUI implements Runnable {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater (new GUI());
-    }
-    public void run() {
-        // window
-        JFrame f = new JFrame("Hello, World!");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(new JLabel("Hello World"));
-        f.setLocation(500, 500);
-        //add button
-        JButton button = new JButton();
+public final class GUI extends JFrame {
+    private static JButton button = new JButton();
+
+    private GUI() {
+        super("doggo");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel banner = new JLabel("Hello World");
+//        add(new JLabel("Hello World"));
+        banner.setForeground(Color.yellow);
+        add(new JColorChooser(banner.getForeground()));
+        setLocation(500, 500);
+
+        JLayeredPane pane = getLayeredPane();
+//        Figure figure = new Figure((Color.green));
         button.addActionListener(new ButtonEventListener());
         button.setText("woof");
-//        button.setBounds(20, 20, 20, 20);
+        button.setBounds(60, 60, 60, 20);
 
-        f.add(button);
-        f.pack();
-        f.setVisible(true);
+//        figure.setBounds(20, 10, 40, 60);
+//        pane.add(figure, JLayeredPane.PALETTE_LAYER);
+        pane.add(button, JLayeredPane.POPUP_LAYER);
+
+        setSize(300, 300);
+        setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+//        run();
+        new GUI();
     }
     class ButtonEventListener implements ActionListener {
-
         @Override
         public void actionPerformed(ActionEvent e) {
             String message = "meow";
---
+++ src/Pane.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Pane extends JFrame {
+    public Pane() {
+//        JLayeredPane pane = getLayeredPane();
+//        f.add(figure, JLayeredPane.POPUP_LAYER);
+//        f.add(button, JLayeredPane.PALETTE_LAYER);
+    }
+}
